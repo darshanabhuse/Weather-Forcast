c71a75056b4381c2862f734e4080e6cc0fc31ed8
Darshana Shivaji Bhuse (Digital) <DA359524@wipro.com>
2019-03-01 15:17:53 +0530

    Updating README.md file

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-# Weather Forcast
-This is a AngularJs project.
+#Weather Forcast
+This is a React.Js project built to look further into stateless functional components in React.
 
-It project is a weather application that calls the openweathermap api to render the 5 day weather forecast for a location.
+It project is a weather application that calls the openweathermap api to render the conditions of supplied countries and cities.
 
+#Hosting Details:
+
+This Weather Forecast app is hosted on Heroku public Web Server.
+
+Hosted URL: https://weather-forecast-app-react.herokuapp.com/
+
+Git Repository Name: Weather-Forcast
+
+Branch Name: weatherForcastJquery
+
+Installation:
+git clone
+git checkout to branch "weatherForcastJquery"
+open index.html
+
+About Code:
+1. Provide an option for user to choose location of their choice by City and Country.
+2. Revisit the code to improve performance.
+3. Display higher-level of weather information such as Location, Minimum Temperature, Humidity, icon for weather and Description.
+5. Handling Error.
+6. Applying Loader while completing Ajax call.
--- js/script.js
+++ js/script.js
@@ -26,6 +26,26 @@ $(document).ready(function(){
                     $('.weatherDeatilsRow').append(trHTML);
                     
                     $('.weatherDeatilsWrapper').removeClass('hide');
+                },
+                error: function (jqXHR, exception) {
+                    var msg = '';
+                    $('.loaderDiv').addClass('hide');
+                    if (jqXHR.status === 0) {
+                        msg = 'Not connect.\n Verify Network.';
+                    } else if (jqXHR.status == 404) {
+                        msg = 'Requested page not found. [404]';
+                    } else if (jqXHR.status == 500) {
+                        msg = 'Internal Server Error [500].';
+                    } else if (exception === 'parsererror') {
+                        msg = 'Requested JSON parse failed.';
+                    } else if (exception === 'timeout') {
+                        msg = 'Time out error.';
+                    } else if (exception === 'abort') {
+                        msg = 'Ajax request aborted.';
+                    } else {
+                        msg = 'Uncaught Error.\n' + jqXHR.responseText;
+                    }
+                    $('.error').removeClass('hide').find('.weather__error').text(msg);
                 }
             });
         }
